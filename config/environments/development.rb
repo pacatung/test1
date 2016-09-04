d309980b677ba457f856c4fed40460cccca63537
BigBoss <pacatung@gmail.com>
2016-09-05 02:59:13 +0800

    user can upload image to s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # for AWS
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    :bucket =>'northcali1',
+    :s3_host_name => 's3-us-west-1.amazonaws.com'
+    }
+  }
 end
